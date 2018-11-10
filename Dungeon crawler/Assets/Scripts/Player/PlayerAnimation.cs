a0e018b4e333ae1a59de3a58cb6b9a7a8981b80f
Arracias <shpakovnikita1998@gmail.com>
2018-11-11 00:02:58 +0300

    Basic enemies and behaviour

---
+++ Dungeon crawler/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private Animator _swordAnimation;
+
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _swordAnimation = transform.GetChild(1).GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Move(float move)
+    {
+        _animator.SetFloat("Move", move);
+    }
+
+    public void Jump(bool jumping)
+    {
+        _animator.SetBool("Jumping", jumping);
+    }
+
+    public void Attack()
+    {
+        _animator.SetTrigger("Attack");
+        _swordAnimation.SetTrigger("SwordAnimation");
+    }
+}
